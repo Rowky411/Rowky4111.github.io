02335b3cc30160b74a4d1bd8720cf9d7385d8130
Rowky411 <rowky4111@gmail.com>
2026-05-14 18:22:15 +0600

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Rowky411.github.io',
-  base: '/Rowky4111.github.io',
 });
